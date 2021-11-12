94c7dc6d4e8f5cd1a2986244d9309f2f0de7862d
Steven Wittens <steven@acko.net>
2021-11-12 21:51:56 +0100

    glsl lezer grammar

--- packages/shader/src/grammar/glsl.test.ts
+++ packages/shader/src/grammar/glsl.test.ts
@@ -2,7 +2,7 @@ import { SyntaxNode } from '@lezer/common';
 import { parser } from './glsl';
 
 const formatASTNode = (node: SyntaxNode) => {
-  const {type, length} = node;
+  const {type} = node;
   let child = node.firstChild;
   let inner = [] as string[];
   while (child) {
@@ -13,14 +13,23 @@ const formatASTNode = (node: SyntaxNode) => {
   return `(${type.name}${space}${inner.join(" ")})`;
 }
 
-const formatAST = (node: SyntaxNode, depth: number = 0) => {
-  const {type, length} = node;
+const formatAST = (node: SyntaxNode, program: string, depth: number = 0) => {
+  const {type, from ,to} = node;
   const prefix = '  '.repeat(depth);
 
   let child = node.firstChild;
-  let out = [`${prefix}${type.name}`];
+  
+  const text = program.slice(node.from, node.to).replace(/\n/g, "⮐")
+  let out = [] as string[];
+  
+  let line = `${prefix}${type.name}`;
+  const n = line.length;
+  line += ' '.repeat(60 - n);
+  line += text;
+  out.push(line);
+
   while (child) {
-    out.push(formatAST(child, depth + 1));
+    out.push(formatAST(child, program, depth + 1));
     child = child.nextSibling;
   }
   return out.join("\n");
@@ -29,24 +38,42 @@ const formatAST = (node: SyntaxNode, depth: number = 0) => {
 `
 float foo = 1.0;
 #define WAT
-int bar = wat(5, 6);
+
 struct light {
  float intensity;
  vec3 position;
 } lightVar;
+void main();
 void main() {
+  int bar = wat(5, 6);
+  int x = 4 + 5 + +6;
+  struct s { } x;
+  gl_FragColor = vec4(0.1, 0.2, 0.3, 1.0);
+}
+`
+
+const PROGRAM = `
+float foo = 1.0;
+#define WAT
+
+struct light {
+ float intensity;
+ vec3 position;
+} lightVar;
+void main();
+void main() {
+  int bar = wat(5, 6);
+  int x = 4 + 5 + +6;
+  struct s { } x;
   gl_FragColor = vec4(0.1, 0.2, 0.3, 1.0);
 }
 `;
 
-const PROGRAM = `
-float foo = 1.0;
-`;
-
 describe("glsl parser", () => {
   it("parses", () => {
-    const parsed = parser.parse(PROGRAM);
+    const text = PROGRAM;
+    const parsed = parser.parse(text);
     console.log(formatASTNode(parsed.topNode));
-    console.log(formatAST(parsed.topNode));
+    console.log(formatAST(parsed.topNode, text));
   });
 });

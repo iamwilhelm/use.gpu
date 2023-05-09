49c9bd145175f1b2b5a113e1821521a8d0fe633d
Steven Wittens <steven@acko.net>
2023-05-09 15:16:02 +0200

    update fallback message

--- packages/app/src/fallback.tsx
+++ packages/app/src/fallback.tsx
@@ -5,7 +5,8 @@ export const FALLBACK_MESSAGE = (error: Error) => <>
   <div className="help-message">
     <p><b>To enable WebGPU:</b></p>
     <ul>
-      <li><b>Chrome</b> – Dev version required<br />Turn on <code>#enable-unsafe-webgpu</code> in <code>chrome://flags</code></li>
+      <li><b>Chrome 113+</b> – Windows, MacOS, ChromeOS ✅</li>
+      <li><b>Chrome</b> – Linux, Android - Dev version required<br />Turn on <code>#enable-unsafe-webgpu</code> in <code>chrome://flags</code></li>
       <li><b>Firefox</b> – Nightly version required<br />Turn on <code>dom.webgpu.enabled</code> in <code>about:config</code></li>
     </ul>
     <p>See <a href="https://caniuse.com/webgpu">CanIUse.com</a> for more info.</p>

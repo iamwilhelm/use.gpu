0f9d1f9967f3198fa3406e748273f6a09286f374
Steven Wittens <steven@acko.net>
2023-05-21 00:02:58 +0200

    types

--- packages/inspect/src/components/fiber.tsx
+++ packages/inspect/src/components/fiber.tsx
@@ -214,7 +214,7 @@ export const FiberNode: React.FC<FiberNodeProps> = memo(({
   const depends  = hoverState.deps.indexOf(fiber.id) >= 0 || (hoverState.root === fiber);
   const precedes = hoverState.precs.indexOf(fiber.id) >= 0 || (yeeted?.root === hoverState.fiber && yeeted.value !== undefined);
   const quoted   = hoverState.fiber?.quote?.to === fiber || fiber?.quote?.to === hoverState.fiber;
-  const unquoted = hoverState.fiber?.unquote?.from === fiber.id || (hoverState.fiber && fiber?.unquote?.from === hoverState.fiber?.id);
+  const unquoted = hoverState.fiber?.unquote?.from === fiber.id || !!(hoverState.fiber && fiber?.unquote?.from === hoverState.fiber?.id);
 
   // Resolve depth-highlighting
   const subnode = hoverState.by ? isSubNode(hoverState.by, fiber) : true;
@@ -228,7 +228,7 @@ export const FiberNode: React.FC<FiberNodeProps> = memo(({
 
   // Make click/hover handlers
   const [select, hover, unhover] = useMemo(() => {
-    const root = () => fiber.yeeted && fiber.type === YEET ? yeeted.root : null;
+    const root = () => fiber.yeeted && fiber.type === YEET ? fiber.yeeted.root : null;
 
     const select  = () => updateSelectState({ $set: fiber });
     const hover   = () => updateHoverState({ $set: {
--- packages/inspect/src/components/types.ts
+++ packages/inspect/src/components/types.ts
@@ -6,10 +6,10 @@ export type PingState = Record<number, number>;
 export type SelectState = LiveFiber<any> | null;
 export type HoverState = {
   fiber: LiveFiber<any> | null,
-  root: LiveFiber<any> | null,
-  deps: LiveFiber<any>[],
-  precs: LiveFiber<any>[],
   by: LiveFiber<any> | null,
+  root: LiveFiber<any> | null,
+  deps: number[],
+  precs: number[],
   depth: number,
 };
 export type OptionState = {

b6866c8843d078bab1b615c8799c0ccb5c3ed64d
Steven Wittens <steven@acko.net>
2023-06-25 22:17:36 +0200

    defs

--- packages/wgsl/src/plot/surface.wgsl.d.ts
+++ packages/wgsl/src/plot/surface.wgsl.d.ts
@@ -2,6 +2,7 @@ declare module "@use-gpu/wgsl/plot/surface.wgsl" {
   type ParsedBundle = import('@use-gpu/shader').ParsedBundle;
   const __module: ParsedBundle;
   export const getSurfaceIndex: ParsedBundle;
+  export const getSurfaceUV: ParsedBundle;
   export const getSurfaceNormal: ParsedBundle;
   export default __module;
 }

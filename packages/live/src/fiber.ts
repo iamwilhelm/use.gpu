09372f8a9750689987286a174fbf597a91bcdf61
Steven Wittens <steven@acko.net>
2024-01-01 08:43:54 +0100

    jsx

--- packages/live/src/fiber.ts
+++ packages/live/src/fiber.ts
@@ -283,7 +283,7 @@ export const reactInterop = (element: any, fiber?: LiveFiber<any>) => {
     if (by != null) {
       const {children} = props;
       if (children) {
-        if (Array.isArray(children)) children.forEach((c: any) => c ? BY_MAP.set(c, by) : null);
+        if (Array.isArray(children)) children.forEach((c: any) => c && typeof c !== 'string' ? BY_MAP.set(c, by) : null);
         else if (typeof children === 'object' && 'props' in children) BY_MAP.set(props.children, by);
       }
 

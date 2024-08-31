e56051ebfe749c22e0f4a53d1dd26985bc760060
Steven Wittens <steven@acko.net>
2024-08-31 23:13:49 +0200

    use swc for js build

--- scripts/build-mjs-cjs.sh
+++ scripts/build-mjs-cjs.sh
@@ -24,23 +24,29 @@ mkdir -p ../../build/mjs/$NPM_PACKAGE 2>/dev/null
 mkdir -p ../../build/packages/$NPM_PACKAGE/mjs 2>/dev/null
 mkdir -p ../../build/packages/$NPM_PACKAGE/cjs 2>/dev/null
 
-MODULE_ENV=mjs babel src --out-dir ../../build/mjs/$NPM_PACKAGE/src --extensions ".ts,.tsx,.js,.jsx" --ignore "src/**/__mocks__/**/*.js" --ignore "src/**/*.test.ts" --ignore "src/**/*.d.ts" 1>/dev/null
-exit_on_error $? babel
+# MJS
+
+swc --config-file ../../swc-mjs.json src --out-dir ../../build/mjs/$NPM_PACKAGE/src --extensions ".ts,.tsx,.js,.jsx" --ignore "src/**/__mocks__/**/*.js" --ignore "src/**/*.test.ts" --ignore "src/**/*.d.ts" 1>/dev/null
+exit_on_error $? swc
 
 if test -n "$(find . -maxdepth 1 -name '*.ts' -print -quit 2>/dev/null)"
 then
-  MODULE_ENV=mjs babel *.ts --out-dir ../../build/mjs/$NPM_PACKAGE --extensions ".ts" 1>/dev/null
-  exit_on_error $? babel
+  swc --config-file ../../swc-mjs.json *.ts --out-dir ../../build/mjs/$NPM_PACKAGE --extensions ".ts" 1>/dev/null
+  exit_on_error $? swc
 fi
 
-MODULE_ENV=cjs babel src --out-dir ../../build/cjs/$NPM_PACKAGE/src --extensions ".ts,.tsx,.js,.jsx" --ignore "src/**/__mocks__/**/*.js" --ignore "src/**/*.test.ts" 1>/dev/null
-exit_on_error $? babel
+# CJS
+
+swc --config-file ../../swc-cjs.json src --out-dir ../../build/cjs/$NPM_PACKAGE/src --extensions ".ts,.tsx,.js,.jsx" --ignore "src/**/__mocks__/**/*.js" --ignore "src/**/*.test.ts" 1>/dev/null
+exit_on_error $? swc
 
 if test -n "$(find . -maxdepth 1 -name '*.ts' -print -quit 2>/dev/null)"
 then
-  MODULE_ENV=cjs babel *.ts --out-dir ../../build/cjs/$NPM_PACKAGE --extensions ".ts" 1>/dev/null
-  exit_on_error $? babel
+  swc --config-file ../../swc-cjs.json *.ts --out-dir ../../build/cjs/$NPM_PACKAGE --extensions ".ts" 1>/dev/null
+  exit_on_error $? swc
 fi
+
+# Bin .js
 
 if test -n "$(find bin -maxdepth 1 -print -quit 2>/dev/null)"
 then
@@ -48,9 +54,13 @@ then
   cp -r ./bin/* ../../build/packages/$NPM_PACKAGE/bin/
 fi
 
+# Use MJS as TS built output
+
 cp -r ../../build/mjs/$NPM_PACKAGE/src ../../build/ts/$NPM_PACKAGE
 
 echo Compiled to .js.
+
+# Emit typedefs .d.ts
 
 rm tsconfig.tsbuildinfo 2>/dev/null
 tsc --emitDeclarationOnly
@@ -63,10 +73,15 @@ cp ../../build/ts/$NPM_PACKAGE/*.js ../../build/packages/$NPM_PACKAGE/ 2>/dev/nu
 
 echo Generated .d.ts.
 
+# Copy CJS back
+
 cp -r ../../build/cjs/$NPM_PACKAGE/src/* ../../build/packages/$NPM_PACKAGE/cjs/
 
-ts-node ../../scripts/rename-mjs-cjs.ts c
-ts-node ../../scripts/rename-mjs-cjs.ts m
+# Rename .js to .cjs / .mjs
+
+ts-node ../../scripts/rename-mjs-cjs.ts
+
+# Finalize package.json
 
 cp README.md package.json ../../build/packages/$NPM_PACKAGE
 

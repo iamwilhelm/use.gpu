a46726521dacd090cfe1d3b7bc4249b578e53a3d
Steven Wittens <steven@acko.net>
2024-09-09 20:22:21 +0200

    overrideable webgpu features

--- packages/webgpu/src/components/webgpu.ts
+++ packages/webgpu/src/components/webgpu.ts
@@ -5,14 +5,26 @@ import { Queue, DeviceContext } from '@use-gpu/workbench';
 
 import { mountGPUDevice } from '../web';
 
+const REQUIRED_EXTENSIONS: GPUFeatureName[] = [];
+const OPTIONAL_EXTENSIONS: GPUFeatureName[] = ["rg11b10ufloat-renderable", "depth32float-stencil8", "shader-f16"];
+
 export type ErrorRenderer = (e: Error) => LiveElement;
 
 export type WebGPUProps = PropsWithChildren<{
+  required?: GPUFeatureName[],
+  optional?: GPUFeatureName[],
   fallback: LiveElement | ErrorRenderer,
 }>;
 
-export const WebGPU: LC<WebGPUProps> = ({fallback, children}: WebGPUProps) => {
-  const [result, error] = useAwait(() => mountGPUDevice([], ["rg11b10ufloat-renderable", "depth32float-stencil8", "shader-f16"]), []);
+export const WebGPU: LC<WebGPUProps> = (props: WebGPUProps) => {
+  const {
+    required = REQUIRED_EXTENSIONS,
+    optional = OPTIONAL_EXTENSIONS,
+    fallback,
+    children,
+  } = props;
+
+  const [result, error] = useAwait(() => mountGPUDevice(required, optional), [...required, null, ...optional]);
   useResource((dispose) => {
     if (!result) return;
 

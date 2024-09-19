a3540e7a9a311a4eefbbb0aaa751bd9ede031a0c
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2024-09-19 16:59:39 +0000

    Add fallback information about Safari Technology Preview

--- packages/app/src/fallback.tsx
+++ packages/app/src/fallback.tsx
@@ -8,6 +8,7 @@ export const FALLBACK_MESSAGE = (error: Error) => <>
       <li><b>Chrome 113+</b> – Windows, MacOS, ChromeOS ✅</li>
       <li><b>Chrome</b> – Linux, Android - Dev version required<br />Turn on <code>#enable-unsafe-webgpu</code> in <code>chrome://flags</code></li>
       <li><b>Firefox</b> – Nightly version required<br />Turn on <code>dom.webgpu.enabled</code> in <code>about:config</code></li>
+      <li><b>Safari</b> – macOS, iPadOS, iOS - Technical Preview version required<br />Turn on <code>WebGPU</code> in Settings, Feature Flags</li>
     </ul>
     <p>Note that WebGPU requires an HTTPS connection if not running on <code>localhost</code>.</p>
     <p>See <a href="https://caniuse.com/webgpu">CanIUse.com</a> for more info.</p>
